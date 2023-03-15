900e8db65314be851207cf719b50060dc793bd4a
flying-adv <123318831+flying-adv@users.noreply.github.com>
2023-03-15 16:29:47 +0000

    relative paths

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -1,20 +1,20 @@
 ## Pretrained models paths
-e4e = '/content/drive/MyDrive/zero_shot/PTI/pretrained_models/e4e_ffhq_encode.pt'
-# stylegan2_ada_ffhq = '/content/metfaces.pkl' #'/content/drive/MyDrive/zero_shot/PTI/pretrained_models/ffhq.pkl'
-stylegan2_ada_ffhq = '/content/afhqcat.pkl'
+e4e = './pretrained_models/e4e_ffhq_encode.pt'
+stylegan2_ada_ffhq = './pretrained_models/ffhq.pkl'
+# stylegan2_ada_ffhq = '/content/afhqcat.pkl'
 style_clip_pretrained_mappers = ''
 ir_se50 = './pretrained_models/model_ir_se50.pth'
-dlib = '/content/drive/MyDrive/zero_shot/PTI/pretrained_models/align.dat'
+dlib = './pretrained_models/align.dat'
 
 ## Dirs for output files
 checkpoints_dir = './checkpoints'
 embedding_base_dir = './embeddings'
 styleclip_output_dir = './StyleCLIP_results'
-experiments_output_dir = '/content/drive/MyDrive/zero_shot/PTI/results_paper/smile/output'
+experiments_output_dir = './output/'
 
 ## Input info
 ### Input dir, where the images reside
-input_data_path = '/content/drive/MyDrive/zero_shot/PTI/results_paper/smile/input'
+input_data_path = './results_paper/smile/input'
 ### Inversion identifier, used to keeping track of the inversion results. Both the latent code and the generator
 input_data_id = 'joker'
 
